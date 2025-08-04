1cc388eaa842637370bd7d5b6c43d3801b3ca5ae
Harold Onyebuchi Ekemkpa <haroldonyebuchi507@gmail.com>
2025-08-05 00:44:57 +0100

    addmin completed

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import { createHireMeTable } from "./model/hiremeModel.js";
 import { createTestimonialTable } from "./model/testimonialModel.js";
+import { createAdminTable } from "./model/adminModel.js";
 
 // ROUTES
 import hireRouter from "./routes/hiremeRoute.js";
 import testimonialRouter from "./routes/testimonialRoute.js";
+import adminRouter from "./routes/adminRoutes.js";
 const app = express();
 dotenv.config();
 
@@ -13,9 +15,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 await createHireMeTable();
 await createTestimonialTable();
-
+await createAdminTable();
 app.use("/api/hires", hireRouter);
 app.use("/api/comments", testimonialRouter);
+app.use("/api/admin", adminRouter);
 
 app.get("/", (req, res) => {
   console.log("hello world");
